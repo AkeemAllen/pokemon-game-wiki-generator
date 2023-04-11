575f18432f3fdd6333c91956decddc89433f07cd
Akeem Allen <akeemallen@Akeems-Air.lan>
2023-04-10 22:17:43 -0400

    Updated Encounters now auto added to mkdocs file

--- routes_generator.py
+++ routes_generator.py
@@ -23,6 +23,12 @@ def get_markdown_image_for_pokemon(pokemon_name: str):
     return f"![{pokemon_name}](../../img/pokemon/{file_name}.png)"
 
 
+def get_link_to_pokemon_page(pokemon_name: str):
+    dex_number = pokemon[pokemon_name]["id"]
+    url_route = get_pokemon_dex_formatted_name(dex_number)
+    return f"[{pokemon_name}](/pokemon/{url_route})"
+
+
 def get_encounter_table_columns(max_pokemon_on_single_route):
     table_columns = ["Area", "Pokemon"]
     if max_pokemon_on_single_route == 1:
@@ -36,7 +42,7 @@ def get_encounter_table_columns(max_pokemon_on_single_route):
 
 def map_encounter_to_markdown(encounter_list):
     encounter_list_markdown = map(
-        lambda encounter: f"{get_markdown_image_for_pokemon(encounter[0])}<br/>{encounter[0]}<br/>{encounter[1]}%",
+        lambda encounter: f"{get_markdown_image_for_pokemon(encounter[0])}<br/>{get_link_to_pokemon_page(encounter[0])}<br/>{encounter[1]}%",
         encounter_list,
     )
     encounter_list_markdown = list(encounter_list_markdown)
@@ -78,12 +84,12 @@ def get_encounter_table_rows(encounters):
 def create_encounter_table(route_name: str, route_directory: str, encounters):
     doc = Document("wild_encounters")
     doc.add_header(f"{route_name.capitalize()}", 1)
-    (
-        table_rows,
-        max_number_of_pokemon_on_single_route,
-    ) = get_encounter_table_rows(encounters)
+    table_rows, max_number_of_pokemon_on_single_route = get_encounter_table_rows(
+        encounters
+    )
+
     table_columns = get_encounter_table_columns(max_number_of_pokemon_on_single_route)
-    print(table_columns)
+
     doc.add_table(
         table_columns,
         table_rows,
@@ -102,7 +108,6 @@ def main(wiki_name: str):
         routes_file.close()
 
     mkdoc_routes = mkdocs_yaml_dict["nav"][2]["Routes"]
-
     for route_name, route_properties in routes.items():
         route_directory = f"dist/{wiki_name}/docs/routes/{route_name}"
         if not os.path.exists(route_directory):
@@ -111,6 +116,21 @@ def main(wiki_name: str):
         create_encounter_table(
             route_name, route_directory, route_properties["encounters"]
         )
+
+        route_entry = {
+            route_name.replace("_", " ").capitalize(): [
+                {"Wild Encounters": f"routes/{route_name}/wild_encounters.md"}
+            ]
+        }
+
+        if route_entry not in mkdoc_routes:
+            mkdoc_routes.append(route_entry)
+
+    mkdocs_yaml_dict["nav"][2]["Routes"] = mkdoc_routes
+
+    with open(f"dist/{wiki_name}/mkdocs.yml", "w") as mkdocs_file:
+        yaml.dump(mkdocs_yaml_dict, mkdocs_file, sort_keys=False, indent=4)
+        mkdocs_file.close()
 
 
 if __name__ == "__main__":
