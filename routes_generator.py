566cd271f861970872ff1e3459aeed54680c2a46
Akeem Allen <akeemallen@Akeems-Air.lan>
2023-04-10 21:54:25 -0400

    Creating encounter table adds extra row when number of encounters exceeds 6

--- routes_generator.py
+++ routes_generator.py
@@ -8,12 +8,17 @@ from snakemd import Document
 from utils import get_pokemon_dex_formatted_name
 
 
+with open(f"temp/pokemon.json", encoding="utf-8") as pokemon_file:
+    pokemon = json.load(pokemon_file)
+    pokemon_file.close()
+
+
 def get_markdown_image_for_item(item_name: str):
     return f"![{item_name}](../../img/items/{item_name}.png)"
 
 
 def get_markdown_image_for_pokemon(pokemon_name: str):
-    dex_number = pokebase.pokemon(pokemon_name).id
+    dex_number = pokemon[pokemon_name]["id"]
     file_name = get_pokemon_dex_formatted_name(dex_number)
     return f"![{pokemon_name}](../../img/pokemon/{file_name}.png)"
 
@@ -24,36 +29,61 @@ def get_encounter_table_columns(max_pokemon_on_single_route):
         return table_columns
 
     for i in range(max_pokemon_on_single_route - 1):
-        if i <= 6:
+        if i < 5:
             table_columns.append("&nbsp;")
     return table_columns
 
 
+def map_encounter_to_markdown(encounter_list):
+    encounter_list_markdown = map(
+        lambda encounter: f"{get_markdown_image_for_pokemon(encounter[0])}<br/>{encounter[0]}<br/>{encounter[1]}%",
+        encounter_list,
+    )
+    encounter_list_markdown = list(encounter_list_markdown)
+    return encounter_list_markdown
+
+
 def get_encounter_table_rows(encounters):
     max_number_of_pokemon_on_single_route = 0
-    table_array_for_encounters = []
-    for encounter_type, encounter_list in encounters.items():
-        if len(encounter_list[:-1]) > max_number_of_pokemon_on_single_route:
-            max_number_of_pokemon_on_single_route = len(encounter_list[:-1])
+    table_array_rows_for_encounters = []
+
+    for encounter_type, pokemon_encounter_list in encounters.items():
+        if len(pokemon_encounter_list[:-1]) > max_number_of_pokemon_on_single_route:
+            max_number_of_pokemon_on_single_route = len(pokemon_encounter_list[:-1])
+
+        mapped_encounter_list = map_encounter_to_markdown(pokemon_encounter_list[:-1])
+        extra_encounter_array = []
+        extra_encounter_list = []
+
+        if len(mapped_encounter_list) > 6:
+            extra_encounter_list = mapped_encounter_list[6:]
+            mapped_encounter_list = mapped_encounter_list[:6]
+            extra_encounter_array = [f"", *extra_encounter_list]
 
         encounter_array = [
             f"{get_markdown_image_for_item(encounter_type)}<br/>old rod<br/>{encounters[encounter_type][-1]}",
-            *map(
-                lambda encounter: f"{get_markdown_image_for_pokemon(encounter[0])}<br/>{encounter[0]}<br/>{encounter[1]}%",
-                encounter_list[:-1],
-            ),
+            *mapped_encounter_list,
         ]
-        table_array_for_encounters.append(encounter_array)
-    return (table_array_for_encounters, max_number_of_pokemon_on_single_route)
+
+        table_array_rows_for_encounters.append(encounter_array)
+        if extra_encounter_array:
+            table_array_rows_for_encounters.append(extra_encounter_array)
+
+    return (
+        table_array_rows_for_encounters,
+        max_number_of_pokemon_on_single_route,
+    )
 
 
 def create_encounter_table(route_name: str, route_directory: str, encounters):
     doc = Document("wild_encounters")
     doc.add_header(f"{route_name.capitalize()}", 1)
-    table_rows, max_number_of_pokemon_on_single_route = get_encounter_table_rows(
-        encounters
-    )
+    (
+        table_rows,
+        max_number_of_pokemon_on_single_route,
+    ) = get_encounter_table_rows(encounters)
     table_columns = get_encounter_table_columns(max_number_of_pokemon_on_single_route)
+    print(table_columns)
     doc.add_table(
         table_columns,
         table_rows,
