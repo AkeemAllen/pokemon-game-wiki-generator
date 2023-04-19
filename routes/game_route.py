ca53d2802a6509af34959413c5f4ea23ef5d3981
Akeem Allen <akeemallen@Akeems-MacBook-Air.local>
2023-04-19 11:08:15 -0400

    Added game _route

--- routes/game_route.py
+++ routes/game_route.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 import json
 
+from models.game_route_models import RouteProperties
+
 
 router = APIRouter()
 
@@ -25,5 +27,39 @@ async def get_game_route(route_name: str):
     return routes[route_name]
 
 
-# @router.post("/save-changes/game_route/{route_name}")
-# async def save_route_changes()
+@router.post("/game_route/{route_name}")
+async def create_game_route(route_name: str, route_properties: RouteProperties):
+    with open(f"temp/routes.json", encoding="utf-8") as routes_file:
+        routes = json.load(routes_file)
+        routes_file.close()
+
+    if route_name in routes:
+        return {"message": "Route already exists", "status": 400}
+
+    routes[route_name] = {}
+
+    routes[route_name] = route_properties.dict(exclude_none=True)
+
+    with open(f"temp/routes.json", "w", encoding="utf-8") as routes_file:
+        routes_file.write(json.dumps(routes))
+        routes_file.close()
+
+    return {"message": "Route created", "status": 200}
+
+
+@router.post("/save-changes/game_route/{route_name}")
+async def save_route_changes(route_name: str, route_properties: RouteProperties):
+    with open(f"temp/routes.json", encoding="utf-8") as routes_file:
+        routes = json.load(routes_file)
+        routes_file.close()
+
+    if route_name not in routes:
+        return {"message": "Route not found", "status": 404}
+
+    routes[route_name] = route_properties.dict(exclude_none=True)
+
+    with open(f"temp/routes.json", "w", encoding="utf-8") as routes_file:
+        routes_file.write(json.dumps(routes))
+        routes_file.close()
+
+    return {"message": "Route changes saved", "status": 200}
